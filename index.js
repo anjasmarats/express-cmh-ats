72deb65e8d06c2330e6980a18949f5867849ab65
anjasmarats <atsakunprogramming@gmail.com>
2025-10-14 20:52:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import express from 'express';
 import supabase from './supabaseClient.js';
 import EJS from 'express-ejs-layouts'
 import fileUpload from 'express-fileupload'
-// import path from 'path';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(EJS)
 app.use(fileUpload())
 app.use(express.static("public"))
