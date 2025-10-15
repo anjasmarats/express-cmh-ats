8a9d694ac85a08d61af4ed5a8a0efa82f8f49e44
anjasmarats <atsakunprogramming@gmail.com>
2025-10-15 10:24:41 +0700

    commit

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/', async (req, res) => {
     });
   }
 
-  const isLoggedIn = req.session
+  const isLoggedIn = req.session != null
 
   console.log("data halaman utama",data)
   console.log("isLoggedIn ",req.session)
