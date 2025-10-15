97a2e8767817e0f3bbd86e2083ad04bf47a123b4
anjasmarats <atsakunprogramming@gmail.com>
2025-10-15 21:24:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const cekauth = (req,res) => {
-  console.log("cek auth req.session.user",!req.session.user)
+  console.log("cek auth req.session.user",req.session.user)
   if (!req.session.user) return res.redirect("/")
 }
 
